a1f89e1d194e95bcb014fdd37edab0c8dfc4fd5c
ksylich <okselenkoandrej@gmail.com>
2020-03-03 13:20:41 +0200

    creat cart-item component

--- src/components/cart-dropdown/cart-dropdown.jsx
+++ src/components/cart-dropdown/cart-dropdown.jsx
@@ -1,16 +1,30 @@
 import React from "react";
 
+import { connect } from "react-redux";
+import PerfectScrollbar from "react-perfect-scrollbar";
+
+
+import CartItem from "../cart-item";
 import CustomButton from "../custom-button";
 
 import "./cart-dropdown.scss";
+import 'react-perfect-scrollbar/dist/css/styles.css';
 
-const CartDropdown = () => {
+const CartDropdown = ({ cartItems }) => {
+  const renderCartItem = item => <CartItem key={item.id} item={item} />;
+
   return (
     <div className="cart-dropdown">
-      <div className="cart-items" />
+      <div className="cart-items">
+        <PerfectScrollbar>{cartItems.map(renderCartItem)}</PerfectScrollbar>
+      </div>
       <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
   );
 };
 
-export default CartDropdown;
+const mapStateToprops = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToprops)(CartDropdown);
